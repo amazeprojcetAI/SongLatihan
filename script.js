6ce8a831d91f43981deb4abe8db0c20bebffbefa
amazeprojcetAI <rurusati@gmail.com>
2025-12-27 12:48:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,265 @@
+
+const allSongsData = [
+    // --- AVRIL LAVIGNE ---
+    {
+        "id": "avril_01", "artist": "Avril Lavigne", "title": "I'm With You", "year": 2002, "key": "A",
+        "lyric": "[Intro]\n[A] [F#m] [D] [E]\n\n[Verse 1]\n[A]I'm standing on a [F#m]bridge\n[D]I'm waitin' in the [E]dark\n[A]I thought that you'd be [F#m]here by now\n[D]There's nothing but the [E]rain\n[A]No footsteps on the [F#m]ground\n[D]I'm listening but there's no [E]sound\n\n[Pre-Chorus]\n[F#m]Isn't anyone [D]tryin' to find me?\n[F#m]Won't somebody come [E]take me home?\n\n[Chorus]\nIt's a [A]damn cold night\nTrying to [F#m]figure out this life\nWon't you [D]take me by the hand?\nTake me [E]somewhere new\nI don't know [A]who you are\nBut I... [F#m]I'm with you\n[D]I'm with you... [E]\n\n[Verse 2]\n[A]I'm looking for a [F#m]place\n[D]I'm searching for a [E]face\n[A]Is anybody [F#m]here I know\n[D]'Cause nothing's going [E]right\nAnd [A]everything's a [F#m]mess\nAnd [D]no one likes to be a[E]lone\n\n[Chorus]\nIt's a [A]damn cold night\nTrying to [F#m]figure out this life\nWon't you [D]take me by the hand?\nTake me [E]somewhere new\nI don't know [A]who you are\nBut I... [F#m]I'm with you\n[D]I'm with you... [E] yeah yeah\n\n[Bridge]\nOh, [F#m]why is everything [D]so confusing?\n[F#m]Maybe I'm just [D]out of my mind\n[A]Yeah, yeah, yeah [E]...\n\n[Outro]\n[A]Take me by the hand\n[F#m]Take me somewhere new\n[D]I don't know who you are\nBut [E]I... I'm with you"
+    },
+    {
+        "id": "avril_02", "artist": "Avril Lavigne", "title": "When You're Gone", "year": 2007, "key": "G",
+        "lyric": "[Intro]\n[G] [D/F#] [Em] [C]\n\n[Verse 1]\n[G]I always needed time on my [D/F#]own\n[Em]I never thought I'd need you there when I [C]cry\n[G]And the days feel like years when I'm [D/F#]alone\n[Em]And the bed where you lie is made up on your [C]side\n\n[Pre-Chorus]\nWhen you [Am]walk away\nI count the [C]steps that you take\nDo you [Am]see how much I need you right [D]now?\n\n[Chorus]\nWhen you're [G]gone\nThe pieces of my [D]heart are missin' you\nWhen you're [Em]gone\nThe face I came to [C]know is missin' too\nWhen you're [Am]gone, the words I need to [C]hear\nTo always get me [G]through the day\nAnd make it [D]okay\nI miss [G]you\n\n[Verse 2]\n[G]I've never felt this way be[D/F#]fore\n[Em]Everything that I do reminds me of [C]you\n[G]And the clothes you left, they lie on the [D/F#]floor\n[Em]And they smell just like you\nI love the things that you [C]do\n\n[Chorus]\nWhen you're [G]gone\nThe pieces of my [D]heart are missin' you\nWhen you're [Em]gone\nThe face I came to [C]know is missin' too\nWhen you're [Am]gone, the words I need to [C]hear\nTo always get me [G]through the day\nAnd make it [D]okay\nI miss [G]you"
+    },
+
+    // --- THE CRANBERRIES ---
+    {
+        "id": "cran_01", "artist": "The Cranberries", "title": "Animal Instinct", "year": 1999, "key": "Em",
+        "lyric": "[Intro]\n[Em] [Am7] [C] [D]\n[Em] [Am7] [C] [D]\n\n[Verse 1]\n[Em]Suddenly, [Am7]something has happened to me\n[C]As I was having my cup of [D]tea\n[Em]Suddenly, I was [Am7]feeling depressed\n[C]I was utterly and totally [D]stressed\n[Em]Do you know you made me [Am7]cry? (Woah-oh)\n[C]Do you know you made me [D]die?\n\n[Chorus]\nAnd the [Em]thing that gets to me (thing that gets to me)\nIs you'll [Am7]never really see (never really see)\nAnd the [C]thing that freaks me out (thing that freaks me out)\nIs I'll [D]always be in doubt (always be in)\n\n[Post-Chorus]\n[G]It is the [D]lovely thing that we [C]have\nIt is the [D]lovely thing that we\n[G]It is the [D]lovely thing\nThe ani[C]mal, the animal ins[D]tinct\n\n[Verse 2]\n[Em]So take my hands and [Am7]come with me\n[C]We will change reali[D]ty\n[Em]So take my hands and [Am7]we will pray\n[C]They won't take you a[D]way\n\n[Chorus]\nAnd the [Em]thing that gets to me (thing that gets to me)\nIs you'll [Am7]never really see (never really see)\nAnd the [C]thing that freaks me out (thing that freaks me out)\nIs I'll [D]always be in doubt (always be in)\nThe ani[Em]mal, the animal...\n[Am7]The animal instinct in [C]me [D]"
+    },
+    {
+        "id": "cran_02", "artist": "The Cranberries", "title": "Ode To My Family", "year": 1994, "key": "D",
+        "lyric": "[Intro]\n[D] [Bm] [F#m] [G] (Doo doo doo...)\n[D] [Bm] [F#m] [G] (Doo doo doo...)\n\n[Verse 1]\n[D]Understand the [Bm]things I say\n[F#m]Don't turn away from [G]me\n[D]'Cause I spent half my [Bm]life out there\n[F#m]You wouldn't disa[G]gree\n[D]D'you see me? [Bm]D'you see?\n[F#m]Do you like me? [G]Do you like me standing there?\n[D]D'you notice? [Bm]D'you know?\n[F#m]Do you see me? [G]Do you see me?\nDoes anyone [D]care?\n\n[Chorus]\nUn[D]happiness\nWhere's when I was [Bm]young\nAnd we didn't give a [F#m]damn\n'Cause we were [G]raised\nTo see life as [D]fun\nAnd take it if we [Bm]can\nMy [F#m]mother... my mother she'd [G]hold me\n\n[Verse 2]\n[D]Understand what [Bm]I've become\n[F#m]It wasn't my de[G]sign\n[D]And people every[Bm]where think\n[F#m]Something better than I [G]am\n[D]I miss you... [Bm]I miss 'cause I liked it\n[F#m]'Cause I liked it... [G]When I was out there\n\n[Chorus]\nUn[D]happiness\nWhere's when I was [Bm]young\nAnd we didn't give a [F#m]damn\n'Cause we were [G]raised\nTo see life as [D]fun\nAnd take it if we [Bm]can\nMy [F#m]mother...\nMy mother she'd [G]hold me"
+    },
+    {
+        "id": "cran_03", "artist": "The Cranberries", "title": "Zombie", "year": 1994, "key": "Em",
+        "lyric": "[Intro]\n[Em] [C] [G] [D/F#]\n\n[Verse 1]\n[Em]Another head hangs [C]lowly\n[G]Child is slowly [D/F#]taken\n[Em]And the violence caused such [C]silence\n[G]Who are we mis[D/F#]taken?\n[Em]But you see, it's not me, it's not [C]my family\nIn your [G]head, in your head, they are [D/F#]fighting\nWith their [Em]tanks and their bombs, and their [C]bombs, and their guns\nIn your [G]head, in your head, they are [D/F#]crying\n\n[Chorus]\nIn your [Em]head, in your [C]head\nZombie, [G]zombie, zom[D/F#]bie-ie-ie\nWhat's in your [Em]head? In your [C]head?\nZombie, [G]zombie, zom[D/F#]bie-ie-ie, oh\n\n[Instrumental]\n[Em] [C] [G] [D/F#]\n\n[Verse 2]\n[Em]Another mother's [C]breaking\n[G]Heart is taking [D/F#]over\n[Em]When the violence causes [C]silence\n[G]We must be mis[D/F#]taken\n[Em]It's the same old theme since [C]1916\nIn your [G]head, in your head, they're still [D/F#]fighting\nWith their [Em]tanks and their bombs, and their [C]bombs, and their guns\nIn your [G]head, in your head, they are [D/F#]dying\n\n[Chorus]\nIn your [Em]head, in your [C]head\nZombie, [G]zombie, zom[D/F#]bie-ie-ie\nWhat's in your [Em]head? In your [C]head?\nZombie, [G]zombie, zom[D/F#]bie-ie-ie"
+    },
+
+    // --- SIXPENCE NONE THE RICHER ---
+    {
+        "id": "sixpence_01", "artist": "Sixpence None The Richer", "title": "Kiss Me", "year": 1997, "key": "C",
+        "lyric": "[Intro]\n[C] [Cmaj7] [C7] [F]\n[C] [Cmaj7] [C7] [F]\n\n[Verse 1]\n[C]Kiss me out of the [Cmaj7]bearded barley\n[C7]Nightly beside the [Cmaj7]green, green grass\n[C]Swing, swing, [Cmaj7]swing the spinning step\n[C7]You wear those shoes and [F]I will wear that dress\n\n[Chorus]\n[Dm]Oh, kiss [G]me beneath the [C]milky twi[Am]light\n[Dm]Lead me [G]out on the [C]moonlit [C7]floor\n[Dm]Lift your [G]open hand\n[C]Strike up the [C/B]band and make the [Am]fireflies dance\n[Am/G]Silver moon's [F]sparkling... [G]So, kiss [C]me\n\n[Interlude]\n[Cmaj7] [C7] [F]\n\n[Verse 2]\n[C]Kiss me down by the [Cmaj7]broken tree house\n[C7]Swing me upon its [Cmaj7]hanging tire\n[C]Bring, bring, [Cmaj7]bring your flowered hat\n[C7]We'll take the trail marked on your [F]father's map\n\n[Chorus]\n[Dm]Oh, kiss [G]me beneath the [C]milky twi[Am]light\n[Dm]Lead me [G]out on the [C]moonlit [C7]floor\n[Dm]Lift your [G]open hand\n[C]Strike up the [C/B]band and make the [Am]fireflies dance\n[Am/G]Silver moon's [F]sparkling... [G]So, kiss [C]me\n\n[Outro]\n[Cmaj7] [C7] [F]\nSo kiss me... [C]"
+    },
+    {
+        "id": "sixpence_02", "artist": "The La's / Sixpence", "title": "There She Goes", "year": 1990, "key": "G",
+        "lyric": "[Intro]\n[G] [D] [C] [D]\n[G] [D] [C] [D]\n\n[Verse 1]\n[G]There she [D]goes\n[C]There she goes a[D]gain\n[G]Racing through my [D]brain\nAnd I [C]just can't con[D]tain\nThis [C]feelin' that re[G]mains\n\n[Verse 2]\n[G]There she [D]blows\n[C]There she blows a[D]gain\n[G]Pulsing through my [D]vein\nAnd I [C]just can't con[D]tain\nThis [C]feelin' that re[G]mains\n\n[Chorus]\n[Em]There [C]she goes\n[Em]There she goes a[C]gain... [D]\n\n[Verse 3]\n[G]She calls my [D]name, pulls my [C]train\n[D]No one else could heal my [G]pain\nBut I [D]just can't con[C]tain\nThis [D]feelin' that re[G]mains\n\n[Outro]\n[G]There she [D]goes\n(There she goes again)\n[C]There she [D]goes\n(There she goes again)"
+    },
+
+    // --- INDONESIAN HITS ---
+    {
+        "id": "bernadya_01", "artist": "Bernadya", "title": "Satu Bulan", "year": 2024, "key": "C",
+        "lyric": "[Intro]\n[F] [G] [Em] [Am]\n[Dm] [G] [C]\n\n[Verse 1]\nSudah adakah yang [F]gantikanku\nYang kau [G]antar jemput setiap [Em]sabtu [Am]\nSudah adakah yang [F]menenangkanmu\nSaat kau [G]merasa semuanya [C]musuhmu\n\n[Verse 2]\nSudah adakah yang [F]lebih sabar\nMeghadapi [G]jurang emosimu yang [Em]besar [Am]\nSudah adakah yang [F]buatmu sadar\nDia [G]bukan aku tak perlu kau [C]bandingkan\n\n[Chorus]\n[F]Belum ada satu [G]bulan\nKu yakin masih ada sisa [Em]wangiku di baju[Am]mu\nNamun kau tampak [Dm]baik saja\nTak seperti yang [G]ku kira [C]\n\n[Bridge]\n[F]Mungkin hanya aku [Fm]yang\nTer[Em]luka sendirian [A]\n[Dm]Kau tlah sembuh sedia kala [G]...\n\n[Chorus]\n[F]Belum ada satu [G]bulan\nKu yakin masih ada sisa [Em]wangiku di baju[Am]mu\nNamun kau tampak [Dm]baik saja\nTak seperti yang [G]ku kira [C]\n\n[Outro]\n[F] [G] [Em] [Am]\n[Dm] [G] [C]"
+    },
+    {
+        "id": "mahalini_01", "artist": "Mahalini", "title": "Sial", "year": 2023, "key": "D",
+        "lyric": "[Intro]\n[G] [A] [F#m] [Bm]\n[G] [A] [D]\n\n[Verse 1]\nSampai saat ini tak terpikir [G]olehku\nAku pernah beri rasa pada orang [C]sepertimu [D]\nSeandainya sejak awal tak kuyakinkan [G]diriku [Em]\nApakah sempat pelukmu menenangkan [C]hatiku [D]\n\n[Chorus]\n[G]Bagaimana dengan aku terlanjur men[Bm]cintaimu\nYang datang beri ha[C]rapan, lalu per[D]gi\nDan hilang usai kau sa[C]kiti [D]\nSialnya dia mengubah [Bm]semua [Em]\nMalah kau tuduh aku[Am]lah penyebabnya [D]\nWanita [G]munafik...\n\n[Verse 2]\nPuas kau curangi [G]aku?\nBagaimana dengan [F#m]aku [Bm]\nYang tulus mencinta[Em]i... [F#m]\nYang tulus menyayang[C]i... [A]\n\n[Chorus]\n[G]Bagaimana dengan aku terlanjur men[Bm]cintaimu\nYang datang beri ha[C]rapan, lalu per[D]gi\nDan hilang usai kau sa[C]kiti [D]\nSialnya dia mengubah [Bm]semua [Em]\nMalah kau tuduh aku[Am]lah penyebabnya [D]\nWanita [G]munafik [Gm]...\n\n[Outro]\n[G] [F#m] [Em] [A] [D]"
+    },
+    {
+        "id": "tulus_01", "artist": "Tulus", "title": "Hati-Hati di Jalan", "year": 2022, "key": "F",
+        "lyric": "[Intro]\n[Bb] [C] [Am] [Dm]\n[Gm] [C] [F]\n\n[Verse 1]\nPerja[Bb]lanan memba[C]wamu\nBerte[Am]mu denganku, ku bertemu [Dm]kamu\nSeper[Gm]tinya semua seperti [C]sempurna\nSa[F]yang untuk diakhi[F7]ri\n\n[Verse 2]\nEkspek[Bb]tasi menyata[C]kan\nKita [Am]dirumahkan untuk sa[Dm]ling\nSa[Gm]ling menjaga, saling [C]mencinta\nCari [F]mana ada celah...\n\n[Chorus]\nKu[Bb]kira kita akan ber[C]sama\nBegitu [Am]banyak yang [Dm]sama\nLatar[Gm]mu dan latar[C]ku\nKukira tak[F]kan ada kendala\n\nKu[Bb]kira ini kan mu[C]dah\nKau aku [Am]ada dianta[Dm]ra\nKita [Gm]tahu... ini takkan [Am]mudah\nSelamat [Bb]jalan...\nHati-hati di [C]jalan [F]"
+    }
+];
+
+let songs = allSongsData;
+let currentSong = null;
+let transposeStep = 0;
+let fontSize = 18;
+let scrollInterval = null;
+let isScrolling = false;
+const keys = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'];
+
+const chordShapes = {
+    'C': 'x32010', 'Cm': 'x35543', 'C7': 'x32310',
+    'D': 'xx0232', 'Dm': 'xx0231', 'D7': 'xx0212',
+    'E': '022100', 'Em': '022000', 'E7': '020100',
+    'F': '133211', 'Fm': '133111', 'F#m': '244222',
+    'G': '320003', 'Gm': '355333', 'G7': '320001',
+    'A': 'x02220', 'Am': 'x02210', 'A7': 'x02020',
+    'B': 'x24442', 'Bm': 'x24432', 'Bb': 'x13331'
+};
+
+window.onload = () => { renderList(songs); };
+
+function renderList(data) {
+    const list = document.getElementById('songList');
+    list.innerHTML = '';
+    
+    // Sort A-Z Artist -> Title
+    data.sort((a, b) => {
+        if (a.artist < b.artist) return -1;
+        if (a.artist > b.artist) return 1;
+        return a.title.localeCompare(b.title);
+    });
+
+    if(data.length === 0) list.innerHTML = '<div style="grid-column:1/-1;text-align:center;padding:20px;color:#aaa;">Tidak ada lagu.</div>';
+
+    data.forEach(s => {
+        const div = document.createElement('div');
+        div.className = 'song-card';
+        div.innerHTML = `
+            <div class="meta">
+                <h3>${s.title}</h3>
+                <p>${s.artist}</p>
+            </div>
+            <span class="tag">${s.year}</span>
+        `;
+        div.onclick = () => openPlayer(s);
+        list.appendChild(div);
+    });
+}
+
+function filterSongs() {
+    const q = document.getElementById('searchInput').value.toLowerCase();
+    const filtered = songs.filter(s => 
+        s.title.toLowerCase().includes(q) || 
+        s.artist.toLowerCase().includes(q)
+    );
+    renderList(filtered);
+}
+
+function openPlayer(song) {
+    currentSong = song;
+    transposeStep = 0;
+    fontSize = 18; 
+    stopScroll();
+    
+    document.getElementById('playerModal').style.display = 'flex';
+    document.getElementById('playerTitle').innerText = song.title;
+    document.getElementById('playerArtist').innerText = song.artist;
+    document.body.style.overflow = 'hidden';
+    
+    renderLyrics();
+}
+
+function closePlayer() {
+    stopScroll();
+    document.getElementById('playerModal').style.display = 'none';
+    document.body.style.overflow = 'auto';
+}
+
+function changeFont(delta) {
+    fontSize += delta;
+    if(fontSize < 12) fontSize = 12;
+    if(fontSize > 40) fontSize = 40;
+    renderLyrics();
+}
+
+function renderLyrics() {
+    const container = document.getElementById('lyricsContent');
+    container.style.fontSize = fontSize + 'px';
+    
+    let rootIdx = keys.indexOf(currentSong.key);
+    let currentKeyIdx = (rootIdx + transposeStep) % 12;
+    if (currentKeyIdx < 0) currentKeyIdx += 12;
+    document.getElementById('playerKey').innerText = keys[currentKeyIdx];
+
+    let html = currentSong.lyric;
+    
+    // Format Struktur (Intro, Verse, dll) - HANYA TEXT, BUKAN CHORD
+    const structureRegex = /\[(Intro|Verse.*?|Chorus|Reff|Bridge|Outro|Interlude|Solo|Pre-Chorus|Post-Chorus|Instrumental.*?)\]/gi;
+    html = html.replace(structureRegex, (match) => {
+        let cleanText = match.replace(/[\[\]]/g, '');
+        return `<br><span class="structure-badge">${cleanText}</span><br>`;
+    });
+
+    // Format Chord (Sisanya)
+    html = html.replace(/\[(.*?)\]/g, (match, chord) => {
+        let newChord = getTransposedChord(chord, transposeStep);
+        return `<span class="chord" onclick="showChord('${newChord}')">${newChord}</span>`;
+    });
+    
+    container.innerHTML = html;
+}
+
+function transpose(step) {
+    transposeStep += step;
+    renderLyrics();
+}
+
+function getTransposedChord(chord, step) {
+    let match = chord.match(/^([A-G][#b]?)(.*)$/);
+    if (!match) return chord;
+    let root = match[1];
+    let suffix = match[2];
+    let idx = keys.indexOf(root);
+    if (idx === -1) return chord;
+    let newIdx = (idx + step) % 12;
+    if (newIdx < 0) newIdx += 12;
+    return keys[newIdx] + suffix;
+}
+
+// SCROLL DROPDOWN LOGIC
+function toggleScroll() {
+    const btn = document.getElementById('btnScroll');
+    if (isScrolling) {
+        stopScroll();
+    } else {
+        isScrolling = true;
+        btn.innerHTML = "⏸ Stop";
+        btn.classList.add('active');
+        updateScrollSpeed();
+    }
+}
+
+function updateScrollSpeed() {
+    if(!isScrolling) return;
+    clearInterval(scrollInterval);
+    
+    const delay = parseInt(document.getElementById('speedSelect').value);
+    const wrapper = document.getElementById('lyricsWrapper');
+    
+    scrollInterval = setInterval(() => {
+        wrapper.scrollBy(0, 1);
+    }, delay);
+}
+
+function stopScroll() {
+    isScrolling = false;
+    clearInterval(scrollInterval);
+    const btn = document.getElementById('btnScroll');
+    btn.innerHTML = "▶ Auto Scroll";
+    btn.classList.remove('active');
+}
+
+function showChord(chordName) {
+    const popup = document.getElementById('chordPopup');
+    const canvas = document.getElementById('chordCanvas');
+    const ctx = canvas.getContext('2d');
+    popup.style.display = 'block';
+    
+    let clean = chordName.replace(/[0-9]/g, '').split('/')[0];
+    let pattern = chordShapes[clean] || 'xxxxxx';
+    
+    drawChord(ctx, pattern, chordName);
+    setTimeout(() => popup.style.display = 'none', 3000);
+}
+
+function drawChord(ctx, pattern, name) {
+    ctx.clearRect(0,0,120,140);
+    ctx.fillStyle = "#222";
+    ctx.fillRect(0,0,120,140);
+    const pad=15, w=90, h=100, strGap=w/5, fretGap=h/5;
+    
+    ctx.fillStyle = "#1db954";
+    ctx.font = "bold 16px Arial";
+    ctx.fillText(name, pad, 12);
+
+    ctx.strokeStyle="white"; ctx.lineWidth=3;
+    ctx.beginPath(); ctx.moveTo(pad,20); ctx.lineTo(pad+w,20); ctx.stroke();
+    
+    ctx.lineWidth=1;
+    for(let i=0; i<6; i++) {
+        let x=pad+i*strGap; ctx.beginPath(); ctx.moveTo(x,20); ctx.lineTo(x,120); ctx.stroke();
+    }
+    for(let i=1; i<=5; i++) {
+        let y=20+i*fretGap; ctx.beginPath(); ctx.moveTo(pad,y); ctx.lineTo(pad+w,y); ctx.stroke();
+    }
+    
+    ctx.fillStyle="#1db954";
+    for(let i=0; i<6; i++) {
+        let char=pattern[i]; let x=pad+i*strGap;
+        if(char==='x'){
+            ctx.strokeStyle="red";
+            ctx.beginPath(); ctx.moveTo(x-3,10); ctx.lineTo(x+3,16); ctx.stroke();
+            ctx.beginPath(); ctx.moveTo(x+3,10); ctx.lineTo(x-3,16); ctx.stroke();
+            ctx.strokeStyle="white";
+        } else {
+            let fret=parseInt(char);
+            if(fret>0) {
+                let y=20+(fret*fretGap)-(fretGap/2);
+                ctx.beginPath(); ctx.arc(x,y,6,0,Math.PI*2); ctx.fill();
+            } else {
+                ctx.strokeStyle="white"; ctx.beginPath(); ctx.arc(x,13,3,0,Math.PI*2); ctx.stroke();
+            }
+        }
+    }
+}
